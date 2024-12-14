d6571acbeeb6a934b19a9603ca447a13feead116
msvdev-k <msvdev.k@gmail.com>
2024-12-14 17:21:02 +0300

    Top Interview 150. 98. Validate Binary Search Tree

--- src/main/java/ru/msvdev/leetcode/topInterview150/binarysearchtree/N98ValidateBinarySearchTree.java
+++ src/main/java/ru/msvdev/leetcode/topInterview150/binarysearchtree/N98ValidateBinarySearchTree.java
@@ -3,6 +3,8 @@ package ru.msvdev.leetcode.topInterview150.binarysearchtree;
 import ru.msvdev.leetcode.topInterview150.binarytreegeneral.TreeNode;
 
 /**
+ * 98. Validate Binary Search Tree
+ *
  * Given the root of a binary tree, determine if it is a valid binary search tree (BST).
  * A valid BST is defined as follows:
  * -- The left subtree of a node contains only nodes with keys less than the node's key.
